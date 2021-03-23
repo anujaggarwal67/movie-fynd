f49f5f5d8bea344b992d3fb62f737a04010238dd
Anuj aggarwal <anujaggarwal67@gmail.com>
2021-03-23 22:44:28 +0530

    checking mongodb conection2

--- app.py
+++ app.py
@@ -92,7 +92,9 @@ def login():
         json['email'] = email
         json['password'] = password
         print(password)
+        print(getenv("mongodb_host"))
         print("all movies are ", Movie.objects().to_json())
+
         r = requests.post(url = apiurl+"/api/login", json=json)
         print(apiurl)
         print(r)
@@ -231,21 +233,20 @@ def delete_movie(id):
         flash('there is something wrong','error')
     return redirect(url_for('dashboard'))
 
-if __name__ == '__main__':
-    
-    # to initiliaze the db for the app
-    print("initializing database")
-    initialize_db(app)
 
-    print("initiliazing routes")
-    # to initialize the routes for the apis
-    initialize_routes(api)
+# to initiliaze the db for the app
+print("initializing database")
+initialize_db(app)
 
-    # to provide a simple interface for overriding Werkzeug's built-in password hashing utilities.
-    bcrypt = Bcrypt(app)
+print("initiliazing routes")
+# to initialize the routes for the apis
+initialize_routes(api)
 
-    # secret key of the app
-    app.secret_key = getenv("secret_key")
-    print(app.secret_key)
-    # to test the changes
-    app.run()
+# to provide a simple interface for overriding Werkzeug's built-in password hashing utilities.
+bcrypt = Bcrypt(app)
+
+# secret key of the app
+app.secret_key = getenv("secret_key")
+print(app.secret_key)
+# to test the changes
+app.run()
